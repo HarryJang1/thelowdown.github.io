1360546bfbbee702ac29184879d4103fccf6e294
HarryJang1 <61259262+HarryJang1@users.noreply.github.com>
2021-09-24 00:14:24 +1000

    Backend 2
    Fixed some issues with Next.js
    
    Bugs:
    - Same issues persist.

---
+++ .history/posts/[id]_20210923211110.js
@@ -0,0 +1,55 @@
+import { google } from 'googleapis';
+
+//Back-end
+export async function getServerSideProps() {
+
+    //Authentication
+    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']});
+
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    //Query
+    const {id} = query;
+    const range = 'Sheet1!A${id}:K${id}';
+
+    const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: ProcessingInstruction.env.SHEET_ID,
+        range,
+    })
+
+    //Result
+    const [title, publishingDate, countryCode, region, image, imageCredit, 
+    content, authorName, authorProfilePicture, correspondentForRegion] = response.data.values[0];
+
+
+    return {
+        props: {
+            title,
+            publishingDate,
+            countryCode,
+            region,
+            image,
+            imageCredit,
+            content,
+            authorName,
+            authorProfilePicture,
+            correspondentForRegion
+        }
+    }
+}
+
+//Front-end
+export default function Post({title, publishingDate, countryCode, region, image, 
+imageCredit, content, authorName, authorProfilePicture,correspondentForRegion}) {
+    return <article>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={}></div>
+
+        <image>{image}</image>
+
+        <h3>{publishingDate}</h3>
+
+        <h2>{content}</h2>
+
+    </article>
+}
